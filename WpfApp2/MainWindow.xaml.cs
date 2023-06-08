3d1a2d44c57575b3c68bddbd7534a88dade121ef
nacharvs <r3dinc@yandex.ru>
2023-06-08 14:12:06 +0300

    findSingleDoc

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -46,11 +46,12 @@ namespace WpfApp2
 
         private void list1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //var buff = users.Find(x => x.Name + " " + x.Surname == list1.SelectedItem.ToString());
-            //lb1.Content = buff.Name;
-            //lb2.Content = buff.Surname;
-            //lb3.Content = buff.SecondName;
-            //lb4.Content = buff.Vacancy;
+
+            var buff = MongoDBExamples.FindDocument(list1.SelectedItem.ToString());
+            lb1.Content = buff.Name;
+            lb2.Content = buff.Surname;
+            lb3.Content = buff.SecondName;
+            lb4.Content = buff.Vacancy;
             //foreach (var user in users)
             //{
 
--- WpfApp2/MongoDBExamples.cs
+++ WpfApp2/MongoDBExamples.cs
@@ -22,5 +22,14 @@ namespace WpfApp2
             var users = collection.Find(x => true).ToList();
             return users;           
         }
+
+        public static User? FindDocument(string name)
+        {
+            var mongoClient = new MongoClient("mongodb://localhost");
+            var database = mongoClient.GetDatabase("TestDatabase");
+            var collection = database.GetCollection<User>("Users");
+            var users = collection.Find(x => x.Name == name).FirstOrDefault();
+            return users;
+        }
     }
 }
